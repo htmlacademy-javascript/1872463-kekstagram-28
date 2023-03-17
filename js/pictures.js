3faae6577406ffff5cbe73166ff270ecc206dafa
Konstantin Zaytsev <kgzaicev@gmail.com>
2023-03-17 15:08:25 +0300

    Выполняет первую часть ДЗ 8-го модуля.

--- js/pictures.js
+++ js/pictures.js
@@ -1,3 +1,4 @@
+import {openModal} from './big-picture.js';
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const picturesFragment = document.createDocumentFragment();
 const picturesContainer = document.querySelector('.pictures');
@@ -7,9 +8,14 @@ const renderPictures = (photos) => {
   photos.forEach((photo) => {
     const photoElement = pictureTemplate.cloneNode(true);
     photoElement.querySelector('.picture__img').src = photo.url;
+    photoElement.querySelector('.picture__img').alt = photo.description;
     photoElement.querySelector('.picture__comments').textContent = photo.comments.length;
     photoElement.querySelector('.picture__likes').textContent = photo.likes;
     picturesFragment.append(photoElement);
+    photoElement.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      openModal(photo);
+    });
   });
   picturesContainer.append(picturesFragment);
 };
